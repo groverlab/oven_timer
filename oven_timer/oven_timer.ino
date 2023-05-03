24ca62678f207030df2efdd81de3e2232726a437
William H. Grover <wgrover@gmail.com>
2023-05-03 10:06:54 -0700

    added IR remote

--- oven_timer/oven_timer.ino
+++ oven_timer/oven_timer.ino
@@ -1,21 +1,28 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
+#include <IRremote.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 Servo myservo;
 
 void setup() {
-  lcd.begin(16,2);
+  Serial.begin(9600);
+  lcd.begin(16, 2);
   myservo.attach(9);
+  IrReceiver.begin(7);
 }
 
 void loop() {
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Hello BIEN 010!");
-  lcd.setCursor(0,1);
-  lcd.print(millis()/1000);
-//  myservo.write(0);
-//  delay(1000);
-//  myservo.write(180);
-//  delay(1000);
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+  if (IrReceiver.decode()) {
+    IrReceiver.resume();
+    Serial.println(IrReceiver.decodedIRData.command);
+  }
+  //  myservo.write(0);
+  //  delay(1000);
+  //  myservo.write(180);
+  //  delay(1000);
 }
